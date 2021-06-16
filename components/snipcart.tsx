f6937bf8120f3bda57f14b070505f31b43e0bf55
Juan Villacorta <jvillacorta@norkut.com.ve>
2021-06-16 02:29:40 -0400

    Speed

--- components/snipcart.tsx
+++ components/snipcart.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useEffect, useState, useContext, createContext, ReactNode } from 'react'
+import scriptLoader from 'react-async-script-loader'
 
 const URL = 'https://cdn.snipcart.com/themes/v3.1.0/default/snipcart.js'
 const API_KEY = 'OTM1ODBiOTAtMDdhYy00NzVmLWJkMGUtYjZmNTgwODVlYTYxNjM3Mzk2ODgxNzczODQxNjUx'
@@ -9,23 +10,6 @@ export const SnipcartHead = () => (
     <link rel="preconnect" href="https://app.snipcart.com" />
     <link rel="preconnect" href="https://cdn.snipcart.com" />
     <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.1.0/default/snipcart.css" />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-        document.addEventListener("DOMContentLoaded", function(event) {
-          setTimeout(function() {
-              var script = document.createElement('script');
-              script.type = 'text/javascript';
-              script.async = true;
-              script.src = "${URL}"
-
-              var s = document.getElementsByTagName('script')[0];
-              s.parentNode.insertBefore(script, s);
-          }, 0)
-        });
-        `,
-      }}
-    />
   </Head>
 )
 
@@ -39,12 +23,12 @@ export const SnipcartWrapper = () => (
   />
 )
 
-const Snipcart = () => (
+const Snipcart = scriptLoader([URL])(() => (
   <div style={{ position: 'fixed', zIndex: 9999 }}>
     <SnipcartHead />
     <SnipcartWrapper />
   </div>
-)
+))
 
 export const snipcartContext = createContext<{
   cartQty: number
